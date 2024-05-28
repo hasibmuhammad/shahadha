9fbec497be778fe98e4a6ca32cc76d31662d29d1
hasibmuhammad <hasibmu99@gmail.com>
2024-05-28 14:42:56 +0600

    hero shop now button fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import model from "../../public/model.webp";
 import model2 from "../../public/model2.webp";
+import Link from "next/link";
 const Hero = () => {
   return (
     <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-10 lg:gap-0 px-10 lg:px-0">
@@ -15,11 +16,18 @@ const Hero = () => {
           to offer you the best products in affordable price. <br /> Just choose
           your products and Order we will deliver to you.
         </p>
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Shop Now
-          </span>
-        </button>
+        <Link
+          target="_blank"
+          href={
+            "https://www.facebook.com/profile.php?id=61559482554618&mibextid=ZbWKwL"
+          }
+        >
+          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Shop Now
+            </span>
+          </button>
+        </Link>
       </div>
       <div className="relative">
         <Image
